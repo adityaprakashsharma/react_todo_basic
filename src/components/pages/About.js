b5539f6a524d0d57859c46ac3ff10f6e4516869f
Aditya S <aditya.s@calsoftinc.com>
2020-06-20 03:07:17 +0530

    Updated for AWS deployment

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,18 @@ function About() {
         <React.Fragment>
             <h1>About</h1>
             <p>
-                This is the TodoList app 
+                This is the TodoList React App tutorial. Thanks to Traversy media for great tutorial.
+                Tutorial consists of following topics
+                <ul>
+                    <li>What is React ?</li>
+                    <li>Why to use React</li>
+                    <li> Setup basic todo list</li>
+                    <li> State in React</li>
+                    <li> How to set State</li>
+                    <li> Actions like add / delete todo item</li>
+                    <li> Router in React</li>
+                    <li>Axios for Http calls</li>
+                </ul>
             </p>
         </React.Fragment>
             
